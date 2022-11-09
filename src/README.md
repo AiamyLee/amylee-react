ef9c02e5f80c9482b0f8c9535f75acbc691f5286
lichunhui@moseeker.com <lichunhui@moseeker.com>
2022-11-09 16:12:21 +0800

    save

---
+++ src/README.md
@@ -0,0 +1,13 @@
+# amy-react
+
+## 技术选型
+
+- react-router-dom
+- vite
+- mui
+- tailwind/css + scss
+- husky what is?
+
+### 构建
+
+- yarn dev
---
+++ src/router/index.ts
@@ -0,0 +1,6 @@
+import { lazy } from 'react';
+import { RouteObject } from 'react-router-dom';
+
+const rootRouter: RouteObject[] = [{}];
+
+export default rootRouter;
